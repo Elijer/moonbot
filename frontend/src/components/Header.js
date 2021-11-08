95850ddfee3853b5f1b713ec505dd0b7b989198b
Jah <kennedye2@vcu.edu>
2021-11-08 16:59:51 -0600

    mobile styling

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -6,8 +6,6 @@ import AuthContext from '../context/AuthContext'
 const Header = () => {
     let { user, logoutUser, loginAttempt} = useContext(AuthContext)
 
-    //let [in, setIn] = useState(user ? "in" : "out")
-
     return (
             <div className = {"nav-bar " + (user ? "in" : "out")}>
                 {/* <Link to="/" className = "nav-option">Home</Link> */}
@@ -27,11 +25,14 @@ const Header = () => {
 
                     <React.Fragment>
 
-                            <Link to="/login" className = "nav-option" >Login</Link>
-                            <Link to="/register" className = "nav-option" >Register</Link>
+                            <Link to="/login" className = "nav-option" >🌙 Login</Link>
+                            <Link to="/register" className = "nav-option" >✨ Register</Link>
 
                     </React.Fragment>
+                    
                 }
+
+                    {/* <Link to="/" className = "header-title nav-option"> 🌙 MoonBot ✨ </Link> */}
                 
                 {loginAttempt &&
                 <div id = "msg-danger" className="alert alert-danger" role="alert">
--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -8,15 +8,7 @@ const LoginPage = () => {
 
     let { loginUser, loginAttempt } = useContext(AuthContext)
     let [img] = useState(
-        //"https://images.unsplash.com/photo-1483221186507-3cfe60ffb2ad?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1632&q=80"
-        //"https://images.unsplash.com/photo-1635759287179-76f77c3234a1?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80"
-        "https://images.unsplash.com/photo-1566847124586-fad08845cb5a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80"
-        //"https://images.unsplash.com/photo-1521489871110-81dc5a61dbda?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1228&q=80"
-        //"https://images.unsplash.com/reserve/aOcWqRTfQ12uwr3wWevA_14401305508_804b300054_o.jpg?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1176&q=80"
-        //"https://images.unsplash.com/photo-1475359201948-d44193401fae?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
-        //"https://images.unsplash.com/photo-1465173121987-373740a169b3?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80"
-        //"https://images.unsplash.com/photo-1476794162124-6e700c3ddcff?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1678&q=80"
-        //"https://images.unsplash.com/photo-1635840106647-ba49d2c4e3db?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80"
+        "https://images.unsplash.com/photo-1537196369054-c87dc3fb7db0?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1770&q=80"
         )
 
     return (
