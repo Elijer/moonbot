4532e8f1be78f57baf0a2b77c827df97dd27a8b0
Jah <kennedye2@vcu.edu>
2021-11-08 20:16:52 -0600

    built out time state info

--- frontend/src/pages/Tracker.js
+++ frontend/src/pages/Tracker.js
@@ -13,39 +13,50 @@ const Tracker = () => {
     let { user } = useContext(AuthContext)
     let { page } = useParams()
 
-    let [theMoment, setTheMoment] = useState(moment())
-    let [date, setDate] = useState("")
-    let [time, setTime] = useState("")
-    let [DBDate, setDBDate] = useState("")
+    let [time, setTime] = useState({
+        time: "",
+        date: "",
+        dateString: "",
+        timeOfDay: ""
+    })
 
     useEffect(()=> {
-
         let m = moment()
-        setTime(m.format('h:mm a'))
-        setDate(m.format('dddd, MMM Do'))
-        setDBDate(m.format('L').replace(/\//g, "-"))
-
-
-    // get date
-/*     let date = theMoment.format('L');
-    let entryDate = date.replace(/\//g, "-");
-    setDate(entryDate) */
-
-
-/*     dateElement.innerHTML = m.format('dddd, MMM */
-/*     timeElement.innerHTML = m.format('h:mm a'); */
-    
-    // Get Time
-    //var timeOfDay = getTimeOfDay(theMoment);
-    //state.timeOfDay = timeOfDay
-
+        setTime({
+            time: m.format('h:mm a'),
+            date: m.format('dddd, MMM Do'),
+            dateString: m.format('L').replace(/\//g, "-"),
+            timeOfDay: getTimeOfDay(m)
+        });
     }, [])
+
+    var getTimeOfDay = function(m){
+        var timeOfDay;
+        var morningStart = moment('2:00am', 'h:mma');
+        var morningEnd = moment('11:59am', 'h:mma');
+        var middayEnd = moment('6:00pm', 'h:mma');
+    
+        if (m.isBefore(middayEnd) && m.isAfter(morningEnd)){
+            timeOfDay = "midday"
+            //midday
+        } else if (m.isBefore(morningEnd) && m.isAfter(morningStart)){
+            timeOfDay = "morning";
+            //morning
+        } else if (m.isAfter(middayEnd)){
+           timeOfDay = "evening";
+            //evening
+        } else {
+            // after midnight
+            timeOfDay = "evening";
+        }
+        return timeOfDay
+    }
 
 
     return (
         <div id = "date">
-            <div id = "day"> {date}</div>
-            <div id = "time"> {time} </div>
+            <div id = "day"> {time.date}</div>
+            <div id = "time"> {time.time} </div>
         </div>
     )
 }
