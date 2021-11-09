40eb80e12eb9518aba95ed57c66856e92f4badc5
Jah <kennedye2@vcu.edu>
2021-11-08 21:19:12 -0600

    organized tracker component style

--- frontend/src/pages/Tracker/TimeDisplay.js
+++ frontend/src/pages/Tracker/TimeDisplay.js
@@ -2,6 +2,7 @@ import React, { useContext} from 'react'
 import AuthContext from '../../context/AuthContext'
 import TimeContext from '../../context/TimeContext'
 import dd from '../../utilities/Debugger'
+import { up } from '../../utilities/Helpers'
 
 import moment from 'moment';
 
@@ -13,7 +14,7 @@ const TimeDisplay = () => {
     return (
         <div id = "date">
             <div id = "day"> {time.date}</div>
-            <div id = "time"> {time.time} </div>
+            <div id = "time"> {up(time.timeOfDay)} </div>
         </div>
     )
 }
---
+++ frontend/src/utilities/Helpers.js
@@ -0,0 +1,3 @@
+export let up = (s) => {
+    return s.charAt(0).toUpperCase() + s.slice(1)
+}
