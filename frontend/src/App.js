3bde32bf61d6fcedaf85d76c5d89db126406b668
Jah <kennedye2@vcu.edu>
2021-11-10 12:12:46 -0600

    added firestore to project

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,8 +10,25 @@ import RegisterPage from './pages/RegisterPage'
 import NotFound from './pages/NotFound'
 import Header from './components/Header'
 
-
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+// Firebase
+import firebase from 'firebase/compat/app'
+import 'firebase/compat/firestore'
+
+import { useCollectionData } from 'react-firebase-hooks/firestore'
+
+firebase.initializeApp = ({
+  apiKey: "AIzaSyCfcluagAp51tAuLFk_-2JqdpGFsZ5t4d4",
+  authDomain: "m00nb0t.firebaseapp.com",
+  projectId: "m00nb0t",
+  storageBucket: "m00nb0t.appspot.com",
+  messagingSenderId: "717345603721",
+  appId: "1:717345603721:web:5a7c8e88fc43f9e97a8658",
+  measurementId: "G-5TJ0NFDXVY"
+});
+
+const db = firebase.firestore()
 
 
 function App() {
