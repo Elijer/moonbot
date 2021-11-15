6a8f4641637540162e5f4579e99a96512e04f7e8
Jah <kennedye2@vcu.edu>
2021-11-15 08:36:45 -0600

    deleted firestore folder

--- frontend/src/context/Firestore/firebaseConfig.js
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-    apiKey: "AIzaSyCfcluagAp51tAuLFk_-2JqdpGFsZ5t4d4",
-    authDomain: "m00nb0t.firebaseapp.com",
-    projectId: "m00nb0t",
-    storageBucket: "m00nb0t.appspot.com",
-    messagingSenderId: "717345603721",
-    appId: "1:717345603721:web:5a7c8e88fc43f9e97a8658",
-    measurementId: "G-5TJ0NFDXVY"
-  };
