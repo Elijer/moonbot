69ac8fbb34ec00c9e1ba36d3a64a3b97874296ae
Jah <kennedye2@vcu.edu>
2021-11-19 14:09:06 -0600

    replaced background image with local file

--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -1,20 +1,18 @@
 import React, { useContext, useState } from 'react'
 import AuthContext from '../context/AuthContext'
 import { Link } from 'react-router-dom'
+import backgroundImage from '../images/pink_sky.jpeg';
 //import dd from '../utilities/Debugger'
 
 
 const LoginPage = () => {
 
     let { loginUser, loginAttempt } = useContext(AuthContext)
-    let [img] = useState(
-        "https://images.unsplash.com/photo-1537196369054-c87dc3fb7db0?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1770&q=80"
-        )
 
     return (
         <div>
             <div id = "bg-color">
-                <img id = "bg-image" src = {img} alt = ""/>
+                <img id = "bg-image" src = {backgroundImage} alt = ""/>
             </div>
             
             <div className = "login">
