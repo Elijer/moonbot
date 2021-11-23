28103206ce659970c09dd32980df2bc3b66d0942
Jah <kennedye2@vcu.edu>
2021-11-23 11:44:43 -0600

    added in some extra code to Config.js, not working yet though

--- frontend/src/context/Config.js
+++ frontend/src/context/Config.js
@@ -1,6 +1,13 @@
+if (window.location.hostname === "localhost") {
+    
+} else {
+
+}
+
 const Config = {
     //serverURL: "http://127.0.0.1:7000/"
-    serverURL: `${window.location.protocol}//${window.location.hostname}:7000/`
+    serverURL: `http://moonbot.me:7000/`
+    //serverURL: `${window.location.protocol}//${window.location.hostname}:7000/`
     //serverURL: "http://http://104.131.76.82:7000/"
 }
 
